f45e99a4e20ada9301ab136497130689790c00ca
Mat Dupont <dupont.mat.l@gmail.com>
2022-03-12 18:16:07 -0500

    splitting vendor and no compression

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -70,16 +70,16 @@ module.exports = {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendor', ///< name of bundle
-          chunks: 'all', ///< type of code to put in this bundle
-          // name(module) {
-          //   // get the name. E.g. node_modules/packageName/not/this/part.js
-          //   // or node_modules/packageName
-          //   const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+          //name: 'vendor', ///< name of bundle
+          //chunks: 'all', ///< type of code to put in this bundle
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
 
-          //   // npm package names are URL-safe, but some servers don't like @ symbols
-          //   return `npm.${packageName.replace('@', '')}`;
-          // },
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
         },
       },
     },
