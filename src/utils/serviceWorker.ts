6f393c9cb272823a054131e1779552911686acb0
Mat Dupont <dupont.mat.l@gmail.com>
2022-03-24 20:32:15 -0400

    bumped to latest version of lhci/cli, removed serviceWorker clgs and added lighthouse asserts

--- src/utils/serviceWorker.ts
+++ src/utils/serviceWorker.ts
@@ -1,14 +1,13 @@
 export const registerServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((registration) => {
-          console.log('SW registered: ', registration);
-        })
-        .catch((registrationError) => {
-          console.log('SW registration failed: ', registrationError);
-        });
+      navigator.serviceWorker.register('/service-worker.js');
+      // .then((registration) => {
+      //   console.info('SW registered: ', registration);
+      // })
+      // .catch((registrationError) => {
+      //   console.log('SW registration failed: ', registrationError);
+      // });
     });
   }
 };
