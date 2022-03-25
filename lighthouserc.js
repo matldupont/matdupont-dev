10c70e9017ca64768b7faae1517bba53c6a50f2b
Mat Dupont <dupont.mat.l@gmail.com>
2022-03-24 20:51:39 -0400

    removed lh collect

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,5 @@
 module.exports = {
   ci: {
-    collect: {
-      headful: true,
-      // settings: {
-      //   emulatedFormFactor: 'desktop',
-      //   throttling: { rttMs: 40, throughputKbps: 10 * 1024, cpuSlowdownMultiplier: 1 },
-      // },
-    },
     upload: {
       target: 'temporary-public-storage',
     },
